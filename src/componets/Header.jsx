00a3ee41f5f2e34115467e75195748b10cf67b0b
harshit-kumar78 <harshit3551@gmail.com>
2023-02-22 21:57:11 +0530

    state added

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
-
+  const [name,setName] = useState('harshit');
   const obj = {color:'red',backgroundColor:'green'}
-  return <h1 style={obj}>Header component created</h1>;
+  const add = 'https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg';
+  const add2 = 'https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg';
+  let bool = true;
+ 
+  return <>
+  <img src={bool?add2:add} style={{height:'200px'}}/>
+  <h1>{name}</h1>
+  <button onClick={()=>setName('dipannita')}>change</button>
+  </>;
 };
 
 export default Header;
