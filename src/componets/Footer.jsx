1f81ad200c15a5d5ebc985fdcbddb1a7b4bba81b
harshit-kumar78 <harshit3551@gmail.com>
2023-02-22 17:35:22 +0530

    component created

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Footer = () => {
+  return <h2>Footer component created</h2>;
+};
+
+export default Footer;
---
+++ src/componets/Paragraph.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Paragraph = () => {
+  return <p>Paragraph component creted</p>;
+};
+
+export default Paragraph;
