859a851b3e4594bb576a4e3d4b0cbed24ffbe410
harshit-kumar78 <harshit3551@gmail.com>
2023-02-22 23:42:03 +0530

    child to parent communication

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./componets/Header";
-import Paragraph from "./componets/Paragraph";
-import Footer from "./componets/Footer";
+import Student from "./componets/Student";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("anil");
-
-  const a = (a, b) => console.log(a + b);
+  const [a, setA] = useState("gigo");
+  function sendData(data) {
+    setA(data);
+  }
   return (
     <div className="App">
-      <h1>props in react </h1>
-      <Header
-        name={name}
-        email="header1@abc.com"
-        address={{ city: "deoghar", state: "jharkhand" }}
-        a={a}
-        set={setName}
-      ></Header>
-
-      {/* <Header
-        name={"header2"}
-        email="header1@abc.com"
-        address={{ city: "deoghar", state: "jharkhand" }}
-      ></Header>
-      <Header
-        name={"header3"}
-        email="header1@abc.com"
-        address={{ city: "deoghar", state: "jharkhand" }}
-      ></Header> */}
+      <p>{a}</p>
+      <Student f={sendData}></Student>
     </div>
   );
 }
---
+++ src/componets/Student.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Student = ({ f }) => {
+  let a = "hashit";
+  return (
+    <>
+      <h1>student component</h1>
+      <button onClick={() => f("harshit")}>change data </button>
+    </>
+  );
+};
+
+export default Student;
