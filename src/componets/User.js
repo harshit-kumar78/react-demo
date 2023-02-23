d69776391843959c83a3dd20f5cdcfad1b2356cf
harshit-kumar78 <harshit3551@gmail.com>
2023-02-23 13:28:23 +0530

    useEffect with clean up code

--- src/componets/User.js
+++ src/componets/User.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const User = ({ data, count }) => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const logMousePosition = (e) => {
+    setX(e.clientX);
+    setY(e.clientY);
+  };
   useEffect(() => {
     console.log("useEffect called");
-  }, [data]);
+    window.addEventListener("mousemove", logMousePosition);
+  }, []);
   return (
     <>
-      <h1>data props :{data}</h1>
-      <h1>count props :{count}</h1>
+      <h1>X:{x}</h1>
+      <h1>Y:{y}</h1>
     </>
   );
 };
